3f9500d05bfc8575dbde5c7e0ce1bf156331059a
pr454d-p <7697prasad@gmail.com>
2023-01-09 11:00:59 +0530

    fix_csrf

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+'https://movieproject.up.railway.app/'
+]
+CORS_ORIGIN_WHITELIST = [
+'https://movieproject.up.railway.app/',
+]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
